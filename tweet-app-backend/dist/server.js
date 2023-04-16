b7fe69828736462aaebf81b54674fe8246673799
ShakiraKP <82374250+ShakaLondon@users.noreply.github.com>
2023-04-16 01:26:25 +0100

    Add prometheus logging

--- tweet-app-backend/dist/server.js
+++ tweet-app-backend/dist/server.js
@@ -6,6 +6,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import bodyParser from "body-parser";
 import swaggerUi from "swagger-ui-express";
+import promBundle from "express-prom-bundle";
 // IMPORT CORS
 import userRouter from "./services/users/index.js";
 import roleRouter from "./services/roles/index.js";
@@ -22,6 +23,7 @@ server.use(cors());
 server.use(express.json());
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
+// Add Swagger
 server.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 server.use("/api/v1_0/tweets/", tweetsRouter);
 server.use("/api/v1_0/tweets/roles", roleRouter);
@@ -29,6 +31,17 @@ server.use("/api/v1_0/tweets/users", userRouter);
 server.use("/api/v1_0/token", tokenRouter);
 // server.use("/api/v1.0/tweets/users/search", searchRouter);
 // MIDDLEWARES
+const metricsMiddleware = promBundle({
+    includeMethod: true,
+    includePath: true,
+    includeStatusCode: true,
+    includeUp: true,
+    customLabels: { project_name: 'hello_world', project_type: 'test_metrics_labels' },
+    promClient: {
+        collectDefaultMetrics: {}
+    }
+});
+server.use(metricsMiddleware);
 server.use(notFoundMiddleware);
 server.use(badRequestMiddleware);
 server.use(unauthorizedMiddleware);
